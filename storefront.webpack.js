8c5346abe277e7cc26af76b5c2e2fbfba0150fe6
Eduardo <edu.vlemes@gmail.com>
2025-02-17 12:06:28 -0300

    bus shipping notes

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -6,7 +6,9 @@ module.exports = () => ({
   resolve: {
     alias: {
    
-      './html/ProductGallery.html': path.resolve(__dirname, 'template/js/custom-js/components/ProductGallery.html')
+      './html/ProductGallery.html': path.resolve(__dirname, 'template/js/custom-js/components/ProductGallery.html'),
+      './html/EcCheckout.html': path.resolve(__dirname, 'template/js/custom-js/components/EcCheckout.html'),
+      './js/EcCheckout.js': path.resolve(__dirname, 'template/js/custom-js/components/EcCheckout.js'),
     }
   }
 })
