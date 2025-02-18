c4458f5242204a286e16e0287c8d279963869b0e
luizabrancaglion <92541373+luizabrancaglion@users.noreply.github.com>
2025-02-18 09:15:20 -0300

    Update checkout.js

--- template/js/checkout.js
+++ template/js/checkout.js
@@ -4,7 +4,7 @@ import ecomCart from '@ecomplus/shopping-cart'
 var lessUnit = document.getElementById('lessUnit')
 var firstphrase = document.getElementById('lessSome')
 var lastphrase = document.getElementById('noMore')
-var lessQuantity = 6
+var lessQuantity = 3
 lessUnit.innerHTML = lessQuantity
 ecomCart.on('change', ({ data }) => {
   var cartCalc = document.querySelectorAll('#cart')
