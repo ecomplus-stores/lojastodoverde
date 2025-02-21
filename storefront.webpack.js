4e0939264288307fb01a5b1fb16794942d29d00b
Eduardo <edu.vlemes@gmail.com>
2025-02-21 16:32:06 -0300

    set big images

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -5,7 +5,7 @@ const pathDslAlias = path.resolve(dirSearchAlias, 'dsl')
 module.exports = () => ({
   resolve: {
     alias: {
-   
+    './html/ProductCard.html': path.resolve(__dirname, 'template/js/custom-js/components/ProductCard.html'),
       './html/ProductGallery.html': path.resolve(__dirname, 'template/js/custom-js/components/ProductGallery.html'),
       './html/EcCheckout.html': path.resolve(__dirname, 'template/js/custom-js/components/EcCheckout.html'),
       './js/EcCheckout.js': path.resolve(__dirname, 'template/js/custom-js/components/EcCheckout.js'),
