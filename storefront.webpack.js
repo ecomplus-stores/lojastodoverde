c2014b4e48f4d7bd8e682ee9b8e37a79f092d3ab
Leonardo Matos <leomp120894@gmail.com>
2025-02-25 18:51:48 -0300

    Hidding buy CTAs without login

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -5,7 +5,6 @@ module.exports = () => ({
     alias: {
       './js/APrices.js': path.resolve(__dirname, 'template/js/custom-js/components/APrices.js'),
       './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/components/APrices.html'),
-      './js/ProductCard.js': path.resolve(__dirname, 'template/js/custom-js/components/ProductCard.js'),
       './html/ProductCard.html': path.resolve(__dirname, 'template/js/custom-js/components/ProductCard.html'),
       './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/components/TheProduct.html'),
       './html/ProductGallery.html': path.resolve(__dirname, 'template/js/custom-js/components/ProductGallery.html'),
