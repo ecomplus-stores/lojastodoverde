e8802a81a223d862ae3b812281a8c4a991a30e40
Vitor Rocha Gonçalves <35348281+vitorrgg@users.noreply.github.com>
2025-03-14 15:03:05 -0300

    Update storefront.webpack.js

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -1,20 +1,19 @@
-
 const path = require('path')
 
 module.exports = () => ({
   resolve: {
-    alias: {
-      /*
-      './js/APrices.js': path.resolve(__dirname, 'template/js/custom-js/components/APrices.js'),
-      './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/components/APrices.html'),
-      
-      './js/ProductCard.js': path.resolve(__dirname, 'template/js/custom-js/components/ProductCard.js'),
+    alias: {              
       './html/ProductCard.html': path.resolve(__dirname, 'template/js/custom-js/components/ProductCard.html'),
-      './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/components/TheProduct.js'),
       './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/components/TheProduct.html'),
       './html/ProductGallery.html': path.resolve(__dirname, 'template/js/custom-js/components/ProductGallery.html'),
       './html/EcCheckout.html': path.resolve(__dirname, 'template/js/custom-js/components/EcCheckout.html'),
-      './js/EcCheckout.js': path.resolve(__dirname, 'template/js/custom-js/components/EcCheckout.js')*/
+      './js/EcCheckout.js': path.resolve(__dirname, 'template/js/custom-js/components/EcCheckout.js')
+      /*
+      './js/ProductCard.js': path.resolve(__dirname, 'template/js/custom-js/components/ProductCard.js'),
+      './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/components/TheProduct.js'),
+      './js/APrices.js': path.resolve(__dirname, 'template/js/custom-js/components/APrices.js'),
+      './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/components/APrices.html'),    
+      */
     }
   }
 })
