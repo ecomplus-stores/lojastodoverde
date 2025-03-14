0852644e750d1064dd97f0174f27761994d8d09f
Vitor Rocha Gonçalves <35348281+vitorrgg@users.noreply.github.com>
2025-03-14 14:44:27 -0300

    Update storefront.webpack.js

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -7,14 +7,14 @@ module.exports = () => ({
       /*
       './js/APrices.js': path.resolve(__dirname, 'template/js/custom-js/components/APrices.js'),
       './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/components/APrices.html'),
-      */
+      
       './js/ProductCard.js': path.resolve(__dirname, 'template/js/custom-js/components/ProductCard.js'),
       './html/ProductCard.html': path.resolve(__dirname, 'template/js/custom-js/components/ProductCard.html'),
       './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/components/TheProduct.js'),
       './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/components/TheProduct.html'),
       './html/ProductGallery.html': path.resolve(__dirname, 'template/js/custom-js/components/ProductGallery.html'),
       './html/EcCheckout.html': path.resolve(__dirname, 'template/js/custom-js/components/EcCheckout.html'),
-      './js/EcCheckout.js': path.resolve(__dirname, 'template/js/custom-js/components/EcCheckout.js')
+      './js/EcCheckout.js': path.resolve(__dirname, 'template/js/custom-js/components/EcCheckout.js')*/
     }
   }
 })
